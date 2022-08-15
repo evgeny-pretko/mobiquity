23b9a233044daa337cd59511a5346768e0859d99
Evgeny Pretko <epretko@efinancialcareers.com>
2022-08-15 09:21:42 +0200

    add email validation

--- src/main/java/utils/EmailValidator.java
+++ src/main/java/utils/EmailValidator.java
@@ -2,8 +2,10 @@ package utils;
 
 public class EmailValidator {
 
+    private static String emailRegex = "^([a-z\\d\\.-_]+)@([a-z\\d-]+)\\.([a-z]{2,8})(\\.[a-z]{2,8})?$";
+
     public static boolean isValidEmail(String email) {
-        return true;
+        return email.toLowerCase().matches(emailRegex);
     }
 
 }
