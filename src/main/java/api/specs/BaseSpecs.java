777f6b6db58a7457fd82dffb93807de4a63478a2
Evgeny Pretko <epretko@efinancialcareers.com>
2022-08-15 22:00:17 +0200

    add config file

--- src/main/java/api/specs/BaseSpecs.java
+++ src/main/java/api/specs/BaseSpecs.java
@@ -3,13 +3,14 @@ package api.specs;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import utils.ConfigProvider;
 
 
 public class BaseSpecs {
 
     public static RequestSpecification requestSpec() {
         return new RequestSpecBuilder()
-                .setBaseUri("https://jsonplaceholder.typicode.com")
+                .setBaseUri(ConfigProvider.BASE_URL)
                 .setContentType(ContentType.JSON)
                 .build();
     }
